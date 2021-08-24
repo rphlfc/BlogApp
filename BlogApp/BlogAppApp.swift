928918f445edcb582855bbbd89db4ee60617fe01
Raphael Cerqueira <raphaelfc@gmail.com>
2021-08-24 09:26:16 -0300

    Initial Commit

---
+++ BlogApp/BlogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppApp.swift
+//  BlogApp
+//
+//  Created by Raphael Cerqueira on 24/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
